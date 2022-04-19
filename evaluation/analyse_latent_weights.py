b5e4b8b30f8861fd5ee4a8335d3ff0e676214820
RKlazinga <r.f.klazinga@student.tudelft.nl>
2022-04-19 10:34:59 +0200

    Add more analysis methods + latex table builder

--- evaluation/analyse_latent_weights.py
+++ evaluation/analyse_latent_weights.py
@@ -1,31 +1,62 @@
 import json
+import math
+import os
+import textwrap
+from statistics import NormalDist
 from typing import Union, List
 
 import matplotlib.pyplot as plt
+import torch
 from torch.nn import BatchNorm1d
 from torch.nn.modules.batchnorm import _BatchNorm
 from tqdm import tqdm
 
+# from evaluation.build_latex import figure_of_runs
 from settings.data_settings import NORMAL_STD_DEV
 from settings.global_settings import RUN_FOLDER
-from settings.train_settings import NETWORK, SPARSITY_PENALTY, LATENT_SPARSITY_PENALTY
-from utils.file import get_topology_of_run, get_epochs_of_run
+from settings.train_settings import NETWORK, CONV_SPARSITY_PENALTY, LINEAR_SPARSITY_PENALTY, LATENT_SPARSITY_PENALTY
+from utils.file import get_topology_of_run, get_epochs_of_run, change_working_dir
 from utils.get_run_id import last_run, last_runs, all_runs_matching
 
 plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.size"] = 18
+LINE_W = 2
 
 
-def analyse_at_epoch(run_id, epoch):
-    model = NETWORK.init_from_checkpoint(run_id, None, None, None, param_epoch=epoch)
-    latent_size, hidden_count, _ = get_topology_of_run(run_id)
+def find_latent_bn(model, topology=None, run_id=None):
+    assert topology or run_id, "Either topology settings or run ID needs to be passed"
+    if topology:
+        latent_size, hidden_count = topology[:2]
+    else:
+        latent_size, hidden_count = get_topology_of_run(run_id)[:2]
     counter = 0
     for m in model.modules():
         if isinstance(m, _BatchNorm):
             counter += 1
             if isinstance(m, BatchNorm1d) and m.weight.data.shape[0] == latent_size:
-                if counter in range(hidden_count + 1, hidden_count + len(model.linear_layers)):
-                    return m.weight.data.tolist(), m.running_mean.tolist(), m.running_var.tolist()
+                linear_layers = 2 if not hasattr(model, "linear_layers") else len(model.linear_layers)
+                if counter in range(hidden_count + 1, hidden_count + linear_layers):
+                    return m
     raise ValueError("Could not find latent neurons")
+
+
+def analyse_all(run_id, epoch):
+    model = NETWORK.init_from_checkpoint(run_id, None, None, None, param_epoch=epoch)
+    weights = []
+    for m in model.modules():
+        if isinstance(m, _BatchNorm):
+            weights.append((m, m.weight.data.tolist()))
+
+    thresh = 2e-4
+    for m, w in weights:
+        zeros = len([0 for w2 in w if abs(w2) < thresh])
+        print(f"{m.__class__.__name__} {len(w)-zeros}/{len(w)} remaining ({(1 - round(zeros / len(w), 3)) * 100}%)")
+
+
+def analyse_at_epoch(run_id, epoch):
+    model = NETWORK.init_from_checkpoint(run_id, None, None, None, param_epoch=epoch)
+    m = find_latent_bn(model, run_id=run_id)
+    return torch.abs(m.weight.data).tolist(), m.bias.data.tolist(), m.running_mean.tolist(), m.running_var.tolist()
 
 
 def plot_analysis_over_time(run_id):
@@ -34,17 +65,25 @@ def plot_analysis_over_time(run_id):
     weights = [tuple([0.1 for _ in range(latent_count)])] + [analyse_at_epoch(run_id, e)[0] for e in range(1, epochs + 1)]
     weights = list(zip(*weights))
 
+    upper_lim = 1e-1
     for w in weights:
-        plt.plot(range(0, epochs+1), w)
+        plt.plot(range(0, epochs+1), w, linewidth=LINE_W)
+        upper_lim = max(upper_lim, max(w))
 
+    print(upper_lim)
     plt.grid(True, linestyle="dashed")
     plt.yscale("log")
 
     plt.ylabel("Weight of latent neuron", labelpad=5)
     plt.gca().set_xlim([-1, epochs + 1])
+    plt.gca().set_ylim([1e-5, 2 * upper_lim])
     plt.gca().xaxis.get_major_locator().set_params(integer=True)
     plt.xlabel("Epoch")
-    plt.title(f"{run_id} std:{NORMAL_STD_DEV} reg:{SPARSITY_PENALTY} latent_reg:{LATENT_SPARSITY_PENALTY}")
+
+    plt.tight_layout()
+    plt.savefig(f"figures/latent_strengths/{run_id}.png")
+
+    plt.title(f"{run_id}\nstd:{NORMAL_STD_DEV} ({CONV_SPARSITY_PENALTY},{LINEAR_SPARSITY_PENALTY},{LATENT_SPARSITY_PENALTY})")
 
     plt.tight_layout()
     plt.show()
@@ -54,28 +93,44 @@ def plot_latent_count_over_time(run_ids: Union[str, List[str]], thresh=2e-4, sho
     if isinstance(run_ids, str):
         run_ids = [run_ids]
 
-    plt.grid(True, linestyle="dashed")
-    plt.ylabel("Number of active latent neurons", labelpad=5)
-    ax1 = plt.gca()
-    ax2 = ax1.twinx()
+    if show:
+        plt.grid(True, linestyle="dashed")
+        ax1 = plt.gca()
+        ax2 = ax1.twinx()
 
     for idx, run_id in enumerate(run_ids):
         epochs = get_epochs_of_run(run_id)
         latent_count = get_topology_of_run(run_id)[0]
-        weights = [analyse_at_epoch(run_id, e)[0] for e in range(1, epochs + 1)]
-        weights = [latent_count] + [len([w2 for w2 in w if w2 > thresh]) + (idx - len(run_ids)//2)/20 for w in weights]
+
+        # get latent weights
+        weights_biases = [analyse_at_epoch(run_id, e)[:2] for e in range(1, epochs + 1)]
+        active_weight_count = [latent_count]
+        for w, b in weights_biases:
+            active_weight_count.append(0)
+            for w2, b2 in zip(w, b):
+                if abs(w2) > thresh:
+                    dist = (NormalDist() * w2) + b2
+                    if 1 - dist.cdf(0) > 0.01:
+                        active_weight_count[-1] += 1
+
+        # weights = [latent_count] + [len([w2 for w2 in w if abs(w2) > thresh]) + (idx - len(run_ids)//2)/20 for w in weights]
+
+        # get loss curve
+        with open(f"{RUN_FOLDER}/{run_id}/loss_graph.json", "r") as readfile:
+            data = json.loads(readfile.read())
+            xs = [d[0] for d in data]
+            ys = [d[2] for d in data]
+
+        # plot both
         if show:
-            ax1.plot(range(0, epochs+1), weights, label="Active latent neurons", alpha=1.0, linewidth=2, snap=True)
+            ax1.plot(range(0, epochs+1), active_weight_count, label="Active latent neurons", alpha=1.0, linewidth=2, snap=True)
+            ax2.plot(xs, ys, color="red", label="Loss")
+        # or return them
+        else:
+            return active_weight_count, ys
 
-            with open(f"{RUN_FOLDER}/{run_id}/loss_graph.json", "r") as readfile:
-                data = json.loads(readfile.read())
-                xs = [d[0] for d in data]
-                ys = [d[2] for d in data]
-                ax2.plot(xs, ys, color="red", label="Loss")
-
-    if not show:
-        return weights
-
+    ax1.yaxis.get_major_locator().set_params(integer=True)
+    ax1.set_ylabel("Number of active latent neurons", labelpad=5)
     ax1.set_xlim([-1, epochs + 1])
     ax1.set_ylim(bottom=-0.25)
     ax2.set_ylim(bottom=0)
@@ -87,20 +142,42 @@ def plot_latent_count_over_time(run_ids: Union[str, List[str]], thresh=2e-4, sho
     h1, l1 = ax1.get_legend_handles_labels()
     h2, l2 = ax2.get_legend_handles_labels()
     plt.legend(h1+h2, l1+l2, loc='upper right')
-    # plt.legend()
-    # plt.title(f"{run_id} std:{NORMAL_STD_DEV} reg:{SPARSITY_PENALTY} latent_reg:{LATENT_SPARSITY_PENALTY}")
+
+    plt.tight_layout()
+    plt.savefig(f"figures/latent_count/{run_id}.png")
+
+    plt.title(f"{run_id}\nstd:{NORMAL_STD_DEV} ({CONV_SPARSITY_PENALTY},{LINEAR_SPARSITY_PENALTY},{LATENT_SPARSITY_PENALTY})")
 
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == '__main__':
-    _run_id = last_run()
-
     # plot_analysis_over_time(_run_id)
-    plot_latent_count_over_time(all_runs_matching("[0.0001,0.0005,0.006]-newer")[0])
-    plot_latent_count_over_time(all_runs_matching("[0.0001,0.005,0.006]-newer")[0])
-    # plot_latent_count_over_time("[0.0001,0.05,0.006]-newer_synthim-[12, 5, 6]-9082a3e2c")
-    # plot_latent_count_over_time("[0.0001,0.005,0.006]-newer_synthim-[12, 5, 6]-9db5763c8")
-    # plot_latent_count_over_time("[0.0001,0.01,0.006]-newer_synthim-[12, 5, 6]-d3bccf2b8")
+    # plot_latent_count_over_time(_run_id)
+
+    for i in range(8):
+        r = last_run(i)
+        # plot_analysis_over_time(r)
+        # plot_latent_count_over_time(r)
+    runs = sorted([last_run(i) for i in range(8)])
+    # figure_of_runs(runs, plot_type="s", label="starter", captioner=lambda x: f"$\lambda={x[1:].split(']')[0]}$")
+
+
+    # mult, bias = analyse_at_epoch(last_runs(1, 3)[0], 15)[:2]
+    # for i, (m, b) in enumerate(zip(mult, bias)):
+    #     if m == 0:
+    #         print(f"{i}) ZERO")
+    #     else:
+    #         dist = (NormalDist() * m) + b
+    #         print(f"{i}) weight {abs(m) > 2e-4} nonclipped {math.ceil(1 - dist.cdf(0) - 1e-3)}")
+    # print(torch.log10(torch.abs(torch.Tensor(analyse_at_epoch(_run_id, 14)[0]))))
+    # analyse_all(_run_id, 5)
+    # analyse_all(_run_id, 26)
+    # print()
+    # analyse_all(_run_id, 20)
+
+    # plot_latent_count_over_time(_run_id)
+    # plot_latent_count_over_time(all_runs_matching("threevar2")[0])
+    # analyse_all(all_runs_matching("e2d4")[0], 20)
     # plot_latent_count_over_time(last_runs(count=3, offset=0))
---
+++ evaluation/build_latex.py
@@ -0,0 +1,160 @@
+import math
+import os
+import shutil
+import textwrap
+import pyperclip
+
+from evaluation.analyse_latent_weights import plot_latent_count_over_time
+from utils.file import change_working_dir
+from utils.get_run_id import all_runs_matching
+
+FIGURE = textwrap.dedent(r"""
+\begin{FIGTYPE}[h!]
+\centering
+SUBFIG
+\caption{CAPTION}
+\label{fig:LABEL}
+\end{FIGTYPE}
+""")
+SUBFIG = textwrap.dedent(r"""\begin{subfigure}[b]{WIDTH\textwidth}
+    \centering
+    \includegraphics[width=\textwidth]{"FNAME"}
+    \caption{CAPTION}
+    \label{subfig:LABEL}
+\end{subfigure}
+""")
+
+
+def table_of_grid_search():
+    dims = [1, 2, 3, 4]
+    lats = [8, 16]
+    for d in [1, 2, 3, 4]:
+        for l_idx, l in enumerate(lats):
+            if l_idx == 0:
+                if d > 1:
+                    print("\cline{0-1}", end="")
+                print(f"\multirow{{{len(lats)}}}{{*}}{{{d}}}", end="")
+            print(f" & {l}", end="")
+            for latent_sparsity in [1e-3, 1e-2, 1e-1, 1]:
+                for linear_sparsity in [1e-3, 1e-2, 1e-1]:
+                    runs = all_runs_matching(f"[{l}-{d}-{latent_sparsity}-{linear_sparsity}]-")
+                    assert len(runs) == 1
+                    count = plot_latent_count_over_time(runs[0], show=False)[0][-1]
+                    if count == d:
+                        count = f"\\textbf{{{count}}}"
+                    print(f" & {count}", end="")
+            print(" \\\\")
+
+def table_of_grid_search_one_col():
+    dims = [1, 2, 3, 4]
+    lat = 8
+    for d in dims:
+        print(f"\multirow{{1}}{{*}}{{{d}}}", end="")
+        for latent_sparsity in [1e-3, 1e-2, 1e-1, 1]:
+            for linear_sparsity in [1e-3, 1e-2, 1e-1]:
+                runs = all_runs_matching(f"[{lat}-{d}-{latent_sparsity}-{linear_sparsity}]-")
+                assert len(runs) == 1
+                count = plot_latent_count_over_time(runs[0], show=False)[0][-1]
+                if count == d:
+                    count = f"\\textbf{{{count}}}"
+                print(f" & {count}", end="")
+        print(" \\\\")
+
+def table_of_grid_search_one_col_flipped():
+    dims = [1, 2, 3, 4]
+    lat = 8
+    observed_shots = None
+    for latent_sparsity in [1e-3, 1e-2, 1e-1, 1]:
+        print(f"\multirow{{1}}{{*}}{{$10^{{{round(math.log10(latent_sparsity))}}}$}}", end="")
+        for d in dims:
+            for linear_sparsity in [1e-3, 1e-2, 1e-1]:
+                runs = all_runs_matching(f"[{lat}-{d}-{latent_sparsity}-{linear_sparsity}]-")
+                if observed_shots is None:
+                    observed_shots = len(runs)
+
+                assert len(runs) == observed_shots
+
+                # get the result for each shot, then take the median
+                assert observed_shots % 2 == 1, "Shot count must be odd to find median"
+                counts = []
+                for r in runs:
+                    single_count = plot_latent_count_over_time(r, show=False)[0][-1]
+                    counts.append(single_count)
+                count = sorted(counts)[observed_shots//2]
+                # count = ",".join(map(str,sorted(counts)))
+                if int(count[2]) == d:
+                    count = f"\\textbf{{{count}}}"
+                if len(set(counts)) > 1:
+                    count = f"\\textit{{{count}}}"
+                print(f" & {count}", end="")
+        print(" \\\\")
+
+
+def figure_of_runs(run_ids, plot_type="c", label="", captioner=None, max_row_width=4, max_fig_width=1/3):
+    change_working_dir()
+
+    assert plot_type in ["c", "s"]
+    if plot_type == "c":
+        plot_type = "latent_count"
+        caption = "Number of active latent neurons and test loss, over training epochs."
+    if plot_type == "s":
+        plot_type = "latent_strengths"
+        caption = "Channel strength of each latent neuron over training epochs."
+
+    r = len(run_ids)
+    if r <= 2:
+        figtype = "figure"
+    else:
+        figtype = "figure*"
+
+    if r <= max_row_width:
+        per_row = r
+        fig_width = 1/r
+    elif (r % 2) == 0 and r <= max_row_width * 2:
+        per_row = max_row_width
+        fig_width = 1 / (r // 2)
+    else:
+        row_count = max_row_width
+            # math.ceil(run_ids / max_row_width)
+        fig_width = 1/max_row_width
+    fig_width = round(min(max_fig_width, fig_width), 3) - 0.01
+
+    subfigs = ""
+    files = []
+    for idx, run_id in enumerate(run_ids):
+        if idx > 0 and idx % per_row == 0:
+            subfigs += "\n"
+        file = f"figures/{plot_type}/{run_id}.png"
+        files.append(file)
+        txt = SUBFIG
+        txt = txt.replace("WIDTH", str(fig_width))
+        txt = txt.replace("FNAME", f"fig/{label}/{run_id}")
+        if captioner:
+            txt = txt.replace("CAPTION", captioner(run_id))
+        else:
+            txt = txt.replace("CAPTION", "")
+        txt = txt.replace("LABEL", f"{label}_{chr(97+idx)}")
+        subfigs += txt
+
+    fig = FIGURE
+    fig = fig.replace("FIGTYPE", figtype)
+    fig = fig.replace("SUBFIG", subfigs)
+    fig = fig.replace("CAPTION", caption)
+    fig = fig.replace("LABEL", label)
+
+    pyperclip.copy(fig)
+
+    input("Enter to collect files for upload...")
+    export_folder = f"export_fig/{label}"
+    if os.path.exists(export_folder):
+        shutil.rmtree(export_folder)
+    os.makedirs(export_folder)
+    for f in files:
+        shutil.copyfile(f, f"{export_folder}/{os.path.basename(f)}")
+    os.startfile(os.path.realpath("export_fig"))
+
+    print(f"Upload the folder '{label}' to 'fig/{label}'")
+
+
+if __name__ == '__main__':
+    table_of_grid_search_one_col_flipped()
