538f8ba056575ff71439d6dc344630ea7b013cd1
RKlazinga <r.f.klazinga@student.tudelft.nl>
2022-04-19 10:42:04 +0200

    Generate example synthetic images for figure

--- datasets/synthetic/im_generators.py
+++ datasets/synthetic/im_generators.py
@@ -1,11 +1,13 @@
 import math
 
 import torch
+from PIL import Image, ImageFont, ImageDraw
 from torchvision.transforms import ToPILImage
 
 from datasets.synthetic.common import r_float
 from models.conv_ae import ConvAE
 from settings.data_settings import NORMAL_STD_DEV, NUM_VARIABLES, CHANNEL_STACKING
+from utils.file import change_working_dir
 
 
 def stacked_sine2d():
@@ -13,9 +15,9 @@ def stacked_sine2d():
 
 
 def sine_2d(std=NORMAL_STD_DEV, num_variables=NUM_VARIABLES):
-    im_size = ConvAE.IMAGE_SIZE
+    im_size = 28
 
-    def sine(x, y, fx=40, fy=40, a=0.5, p=0, bx=im_size//2, by=im_size//2):
+    def sine(y, x, fx=40, fy=40, a=0.5, p=0, sx=0, sy=0, bx=im_size//2, by=im_size//2):
         # circle
         # r = (((x-bx) * fx) ** 2 + ((y-by) * fy) ** 2) ** 0.5 + p
         # horizontal
@@ -23,13 +25,15 @@ def sine_2d(std=NORMAL_STD_DEV, num_variables=NUM_VARIABLES):
         # diagonal
         # r = ((x-bx) * fx + (y-by) * fy) + p
         # square
-        r = max((abs(x-bx)) * fx, (abs(y-by)) * fy) + p
+        r = max((abs(x-bx + sx*(y-by))) * fx, (abs(y-by + sy*(x-bx))) * fy) + p
         return a * math.sin(math.radians(r)) + 0.5
     variables = [
         r_float(30, 100),  # x frequency
         r_float(30, 100),  # y frequency
-        r_float(0.4, 0.7),  # amplitude
+        r_float(0.3, 0.8),  # amplitude
         r_float(360),  # phase
+        r_float(-0.5, 0.5),  # skew x
+        r_float(-0.5, 0.5),  # skew y
         r_float(4, im_size-4),  # x offset
         r_float(4, im_size-4),  # y offset
     ][:num_variables]
@@ -41,6 +45,27 @@ def sine_2d(std=NORMAL_STD_DEV, num_variables=NUM_VARIABLES):
 
 
 if __name__ == '__main__':
-    ToPILImage()(sine_2d(0.01, 6)).show()
-    ToPILImage()(sine_2d(0.01, 6)).show()
-    ToPILImage()(sine_2d(0.01, 6)).show()
+    height = 8
+    dims = 5
+    rim = 8
+    spacing = (2, 12)
+    size = 28
+    font = ImageFont.truetype(r"C:\Windows\Fonts\arial.ttf", size=14)
+    textsize = font.getsize("D=1xx")
+    bg = Image.new("L", (height*28 + (height-1) * spacing[0] + rim + textsize[0], dims*size + 2 * rim + (dims-1) * spacing[1]), color=0)
+    draw = ImageDraw.Draw(bg)
+    for d in range(dims):
+        for i in range(height):
+            im = ToPILImage()(sine_2d(0.01, d))
+            bg.paste(im, (i*(size + spacing[0]) + textsize[0], rim + d*(size + spacing[1])))
+        text = f"D={d}"
+        w = font.getsize(text)[0]
+        draw.text(((textsize[0] - w) // 2, rim + d*(size + spacing[1]) + textsize[1] // 2), text=text, font=font, fill=255)
+    change_working_dir()
+    bg.save("figures/synth_2d_ex.png")
+    # bg.show()
+    # ToPILImage()(sine_2d(0.01, 2)).show()
+    # ToPILImage()(sine_2d(0.01, 3)).show()
+    # ToPILImage()(sine_2d(0.01, 3)).show()
+    # ToPILImage()(sine_2d(0.01, 4)).show()
+    # ToPILImage()(sine_2d(0.01, 4)).show()
