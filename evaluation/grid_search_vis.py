3c152a52e32b394e2329e60099f5ff6d4d30ce20
RKlazinga <r.f.klazinga@student.tudelft.nl>
2022-04-19 10:41:19 +0200

    Expand grid search

--- evaluation/grid_search_vis.py
+++ evaluation/grid_search_vis.py
@@ -11,25 +11,46 @@ from utils.file import change_working_dir
 if __name__ == '__main__':
     change_working_dir()
 
-    res = defaultdict(list)
+    count_res = defaultdict(list)
+    loss_res = defaultdict(list)
 
     for g in tqdm(os.listdir("grid_search")):
-        if "newer" not in g:
-            continue
-        sparsity, latent_s, lr = [float(x) for x in g.split("]")[0].removeprefix("[").split(",")]
-        c = plot_latent_count_over_time(g, show=False)[-1]
-        res[f"non_latent_sparsity={str(sparsity).ljust(5)}"].append(c)
-        res[f"latent_sparsity={str(latent_s).ljust(10)}"].append(c)
-        res[f"learning rate={str(lr).ljust(11)}"].append(c)
+        latent_s, lr = [float(x) for x in g.split("]")[0].removeprefix("[").split(",")]
+        counts, losses = plot_latent_count_over_time(g, show=False)
+        count, loss = counts[-1], losses[-1]
+        # res[f"non_latent_sparsity={str(sparsity).ljust(5)}"].append(c)
+        count_res[f"latent_sparsity={str(latent_s).ljust(10)}"].append(count)
+        loss_res[f"latent_sparsity={str(latent_s).ljust(10)}"].append(loss)
+        count_res[f"learning rate={str(lr).ljust(11)}"].append(count)
+        loss_res[f"learning rate={str(lr).ljust(11)}"].append(count)
 
-    arr = []
-    for k, v in sorted(list(res.items()), key=lambda kv: kv[0][:5]+str(format(float(kv[0].split("=")[1]), ".10f"))):
+    count_arr = []
+    loss_arr = []
+    xlabels = []
+    ylabels = []
+    for k, v in sorted(list(count_res.items()), key=lambda kv: kv[0][:5] + str(format(float(kv[0].split("=")[1]), ".10f"))):
         print(k, v, "avg", str(round(sum(v)/len(v), 2)).ljust(4), "zero count", len([x for x in v if x==0]))
         if k.startswith("latent"):
-            arr.append(v)
+            ylabels.append(k.split("=")[1].strip())
+            count_arr.append(v)
+            loss_arr.append(loss_res[k])
+        else:
+            xlabels.append(k.split("=")[1].strip())
 
-    plt.matshow(np.array(arr))
+    plt.matshow(np.array(count_arr))
     plt.colorbar()
+    plt.gca().set_xticklabels([""] + xlabels)
+    plt.xlabel("Learning Rate")
+    plt.gca().set_yticklabels([""] + ylabels)
+    plt.ylabel("Regularisation")
+    plt.show()
+
+    plt.matshow(np.array(loss_arr))
+    plt.colorbar()
+    plt.gca().set_xticklabels([""] + xlabels)
+    plt.xlabel("Learning Rate")
+    plt.gca().set_yticklabels([""] + ylabels)
+    plt.ylabel("Regularisation")
     plt.show()
 
 
--- experiments/grid_search.py
+++ experiments/grid_search.py
@@ -1,11 +1,29 @@
 from experiments.progressive_mask_drawing import train_and_draw_tickets, main
 from utils.channel_sparsity_reg import update_bn
+from datasets.synthetic.flat_generators import random_sine_gaussian
 
+SHOTS = 2
+conv_sparsity = 1e-4
 
-# for sparsity in [4e-5, 1e-3, 2e-2]:
-sparsity = 1e-4
-for latent_sparsity in [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 2e-1]:
-    for lr in [1e-3, 2e-3, 4e-3, 6e-3, 8e-3, 1e-2]:
-        update_bn.__defaults__ = (sparsity, latent_sparsity)
-        train_and_draw_tickets.__defaults__ = ("grid_search", lr)
-        main(f"[{sparsity},{latent_sparsity},{lr}]-")
+# linear_sparsity = 1e-4
+# latent_sparsity = 1e-4
+# update_bn.__defaults__ = (conv_sparsity, linear_sparsity, latent_sparsity)
+# main(f"[{latent_sparsity}]-")
+
+linear_sparsity = 1e-3
+latent_sparsity = 1e-1
+# for latent_sparsity in [1]:
+# for latent_sparsity in [1e-3, 3e-3, 1e-2, 3e-2, 1e-1]:
+for l in [8]:
+    folder, lr, _ = train_and_draw_tickets.__defaults__
+    train_and_draw_tickets.__defaults__ = (folder, lr, [l, 2, 1])
+    for d in [1, 2, 3, 4]:
+        size, std, num = random_sine_gaussian.__defaults__
+        random_sine_gaussian.__defaults__ = (size, std, d)
+        for latent_sparsity in [1e-3, 1e-2, 1e-1, 1]:
+            for linear_sparsity in [1e-3, 1e-2, 1e-1]:
+                # for lr in [2e-2, 4e-2]: #[1e-4, 1e-3, 2e-3, 4e-3, 1e-2, #3e-2]:
+                #     linear_sparsity = latent_sparsity
+                update_bn.__defaults__ = (conv_sparsity, linear_sparsity, latent_sparsity)
+                for shot in range(SHOTS):
+                    main(f"[{l}-{d}-{latent_sparsity}-{linear_sparsity}]-", [l, 2, 1])
